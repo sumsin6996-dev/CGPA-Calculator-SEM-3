75a0377caaed768bc616d2b4f68f4260dcce7f31
Sumit Singh <sumsin6996@gmail.com>
2026-01-20 23:41:45 +0530

    Added the ADS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -69,6 +70,14 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <Script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7801727099869596"
+                    crossOrigin="anonymous"
+                    strategy="afterInteractive"
+                />
+            </head>
             <body>{children}</body>
         </html>
     )
